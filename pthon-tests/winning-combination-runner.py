f1d849f07c93ed44d5f9435695e24a5f0af67a74
Jenuel Oras Ganawed <me@jenuel.dev>
2025-06-22 16:45:17 +0800

    Update winning-combination-runner.py

--- pthon-tests/winning-combination-runner.py
+++ pthon-tests/winning-combination-runner.py
@@ -1,9 +1,10 @@
 import random
+from typing import Tuple
 
-def generate_sorted_combination(n: int, max_n: int) -> tuple[int]:
+def generate_sorted_combination(n: int, max_n: int) -> Tuple[int, ...]:
     return tuple(sorted(random.sample(range(1, max_n + 1), n)))
 
-def emulate_dynamic_combinations(numberCountPerCombination: int = 6, maxNumber: int = 58, numberOfCombinationToGenerate: int = 1, randomWinningCombination: bool = False):
+def emulate_dynamic_combinations(numberCountPerCombination: int = 6, maxNumber: int = 58, numberOfCombinationToGenerate: int = 1, randomWinningCombination: bool = False, withRandom: bool = False, numberOfRandomCombinations: int = 1):
     count = 0
     print("---------------------------------")
     print("Rolling The Numbers ....")
@@ -16,13 +17,23 @@ def emulate_dynamic_combinations(numberCountPerCombination: int = 6, maxNumber: 
         if randomWinningCombination:
             winning_combinations = {generate_sorted_combination(numberCountPerCombination, maxNumber) for _ in range(numberOfCombinationToGenerate)}
 
+        if withRandom:
+            randomCombination = {generate_sorted_combination(numberCountPerCombination, maxNumber) for _ in range(numberOfRandomCombinations)}
+
         count += 1
         lottery_combination = generate_sorted_combination(numberCountPerCombination, maxNumber)
 
-        if lottery_combination in winning_combinations:
+        if lottery_combination in winning_combinations or (withRandom and lottery_combination in randomCombination):
+            print("---------------------------------")
             print("Combinations:")
             for comb in winning_combinations:
                 print(" - ".join(map(str, comb)))
+            
+            if withRandom:
+                print("Random Combinations:")
+                for comb in randomCombination:
+                    print(" - ".join(map(str, comb)))
+
             print("\nWinning combination:")
             print(" - ".join(map(str, lottery_combination)))
             print(f"Role Times: {count}")
@@ -32,4 +43,4 @@ def emulate_dynamic_combinations(numberCountPerCombination: int = 6, maxNumber: 
 
 # Run the simulation
 for _ in range(100):
-    emulate_dynamic_combinations(6, 49, 12, False)
+    emulate_dynamic_combinations(6, 49, 6, randomWinningCombination=False, withRandom=True, numberOfRandomCombinations=6)
