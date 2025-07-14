288cc9ebb54f1f19acdf95f6a7c290fa28af152a
Jenuel Oras Ganawed <me@jenuel.dev>
2025-07-14 16:32:08 +0800

    Update 6_42.py

--- python-tests/6_42.py
+++ python-tests/6_42.py
@@ -1,14 +1,14 @@
 from winning_combination_runner import emulate_dynamic_combinations
 
 # Run the simulation
-for _ in range(50000):
+for _ in range(5000):
     emulate_dynamic_combinations(
         numberCountPerCombination=6,
         maxNumber=42,
-        numberOfCombinationToGenerate=6,
-        randomWinningCombination=False,
-        withRandom=True,
+        numberOfCombinationToGenerate=18,
+        randomWinningCombination=True,
+        withRandom=False,
         numberOfRandomCombinations=12,
-        runs=10,
+        runs=1,
         printOnlyIfWon=True
     )
